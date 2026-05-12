d32d49b1753718fa2c4c192eaffb5cb8ea578fac
Bruno Herfst <bruno.herfst@gmail.com>
2026-05-12 18:19:05 +1000

    Not supported. Use ofxImGuiVectorEditor

--- src/ofxImGuizmo.h
+++ src/ofxImGuizmo.h
@@ -3,7 +3,6 @@
 #include "ofxImGui.h"
 #include "ImGuizmo.h"
 #include "ImCurveEdit.h"
-#include "ImVectorEditor.h"
 #include "ofCamera.h"
 #include "ofNode.h"
 #include <glm/gtx/matrix_decompose.hpp>
